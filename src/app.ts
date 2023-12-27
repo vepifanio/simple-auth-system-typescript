10ac25628e80f5eb5eecf06c57ddf43065284520
vepifanio <vepifanio1@gmail.com>
2023-12-27 17:55:38 -0300

    feat: `/login (POST)` route and jwt verification for `/users (GET)`  route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import Fastify from 'fastify'
 import { z } from 'zod'
 import { database } from './database'
 import { hash } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = Fastify({ logger: true })
 
@@ -9,6 +13,7 @@ interface User {
   id: string
   email: string
   password: string
+  created_at: string
 }
 
 const createUserRequestBodySchema = z.object({
@@ -42,10 +47,71 @@ app.post('/users', async (request, reply) => {
 })
 
 app.get('/users', async (request, reply) => {
-  const users = await database<User>('users').select('*')
+  const authorizationHeader = request.headers.authorization
+
+  if (!authorizationHeader) {
+    return reply.status(401).send({
+      message: 'Unauthorized',
+    })
+  }
+
+  const accessToken = authorizationHeader.split('Bearer')[1].trim()
+
+  if (!accessToken) {
+    return reply.status(401).send({
+      message: 'Unauthorized',
+    })
+  }
+
+  jwt.verify(accessToken, process.env.JWT_SECRET, (error) => {
+    if (error) {
+      return reply.status(401).send({
+        message: 'Unauthorized',
+      })
+    }
+  })
+
+  const users = await database<User>('users').select(
+    'id',
+    'email',
+    'created_at',
+  )
 
   return reply.send({
     users,
+  })
+})
+
+const loginRequestBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+app.post('/login', async (request, reply) => {
+  const { email, password } = loginRequestBodySchema.parse(request.body)
+
+  const user = await database<User>('users').first().where('email', email)
+
+  if (!user) {
+    return reply.status(401).send({
+      message: 'Unauthorized',
+    })
+  }
+
+  const receivedHashedPassword = await hash(password, user.password)
+
+  if (receivedHashedPassword !== user.password) {
+    return reply.status(401).send({
+      message: 'Unauthorized',
+    })
+  }
+
+  const accessToken = jwt.sign({}, process.env.JWT_SECRET, {
+    subject: user.id,
+  })
+
+  return reply.send({
+    accessToken,
   })
 })
 
