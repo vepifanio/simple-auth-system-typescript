fe3ec1727cebf7b23650ba9c3446d88d3a2f5a1c
vepifanio <vepifanio1@gmail.com>
2023-12-28 12:28:55 -0300

    feat: new variables added to  type

--- src/@types/process-env.d.ts
+++ src/@types/process-env.d.ts
@@ -2,5 +2,9 @@ namespace NodeJS {
   interface ProcessEnv {
     [key: string]: string | undefined
     JWT_SECRET: string
+    POSTGRES_HOST: string
+    POSTGRES_USER: string
+    POSTGRES_PASSWORD: string
+    POSTGRES_DB: string
   }
 }
