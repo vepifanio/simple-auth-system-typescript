4bff0f6a9a0e89550a685cf9a5fb96dfc06cb50a
vepifanio <vepifanio1@gmail.com>
2023-12-29 14:31:03 -0300

    feat: Knex implementation to Users Repository

---
+++ src/database/repository/KnexUsersRepository.ts
@@ -0,0 +1,30 @@
+import { database } from '..'
+import { User } from '../../entity/User'
+import { UsersRepository } from '../../repository/UsersRepository'
+
+export class KnexUsersRepository implements UsersRepository {
+  async findAll(): Promise<User[]> {
+    const users = await database('users').select('id', 'email', 'created_at')
+
+    return users
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await database('users').first().where('email', email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async save(user: User): Promise<void> {
+    await database('users').insert({
+      id: user.id,
+      email: user.email,
+      password: user.password,
+      created_at: user.createdAt,
+    })
+  }
+}
