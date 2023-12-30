55ce829028c140550e5f0020dfd4809ae13c5a31
vepifanio <vepifanio1@gmail.com>
2023-12-29 21:23:27 -0300

    refactor: create a function to handling the jwt validation and added it as a preHandler to the protected route

---
+++ src/auth/jwtAuth.ts
@@ -0,0 +1,31 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import jwt from 'jsonwebtoken'
+
+export async function jwtAuth(request: FastifyRequest, reply: FastifyReply) {
+  const authorizationHeader = request.headers.authorization
+
+  if (!authorizationHeader) {
+    return reply.status(401).send({
+      message: 'Unauthorized.',
+    })
+  }
+
+  const accessToken = authorizationHeader.split('Bearer')[1].trim()
+
+  if (!accessToken) {
+    return reply.status(401).send({
+      message: 'Unauthorized.',
+    })
+  }
+
+  jwt.verify(accessToken, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      return reply.status(401).send({
+        message: 'Unauthorized',
+      })
+    }
+
+    const { sub } = decoded as { sub: string }
+    request.user = sub
+  })
+}
--- src/routes/users/fetch-users.ts
+++ src/routes/users/fetch-users.ts
@@ -2,33 +2,11 @@ import { FastifyInstance } from 'fastify'
 import jwt from 'jsonwebtoken'
 import { FetchUsersUseCase } from '../../application/use-cases/FetchUsers'
 import { KnexUsersRepository } from '../../database/repository/KnexUsersRepository'
+import { jwtAuth } from '../../auth/jwtAuth'
 
 export async function fetchUsers(app: FastifyInstance) {
+  app.addHook('preHandler', jwtAuth)
   app.get('/', async (request, reply) => {
-    const authorizationHeader = request.headers.authorization
-
-    if (!authorizationHeader) {
-      return reply.status(401).send({
-        message: 'Unauthorized',
-      })
-    }
-
-    const accessToken = authorizationHeader.split('Bearer')[1].trim()
-
-    if (!accessToken) {
-      return reply.status(401).send({
-        message: 'Unauthorized',
-      })
-    }
-
-    jwt.verify(accessToken, process.env.JWT_SECRET, (error) => {
-      if (error) {
-        return reply.status(401).send({
-          message: 'Unauthorized',
-        })
-      }
-    })
-
     const usersRepository = new KnexUsersRepository()
     const fetchUsersUseCase = new FetchUsersUseCase(usersRepository)
 
